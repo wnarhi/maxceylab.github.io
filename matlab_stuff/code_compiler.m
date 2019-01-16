245a400790de6b7164b312855bf97528131c518b
PaulScotti <scottibrain@gmail.com>
2019-01-16 14:03:36 -0500

    ge

--- matlab_stuff/code_compiler.m
+++ matlab_stuff/code_compiler.m
@@ -1,119 +1,119 @@
 clc; clear;
-cwd = '/Users/scotti.5/Documents/GitHub/maxceylab.github.io/stimuli/change_detection';
+cwd = '/Users/scotti.5/Documents/GitHub/maxceylab.github.io/stimuli/letters/letter_examples';
 imagefiles = dir(fullfile(cwd, ('*.png')));
 nfiles = length(imagefiles);    % Number of files found
 
 cd output
 
-preset = ['<img id="IDN" src="https://maxceylab.github.io/stimuli/change_detection/Object.png" style="margin-left:-1000px;margin-top:-1000px">'];
+preset = ['<img id="IDN" src="https://maxceylab.github.io/stimuli/letters/Object.png" style="margin-left:-1000px">'];
 code = [];
 for i = 1:nfiles
-    presetNew = strrep(preset,'Object',[imagefiles(i).name]);
-    presetNew = strrep(presetNew,'IDN',[imagefiles(i).name]);
+    presetNew = strrep(preset,'Object',['Letter_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Letter_',num2str(10+i)]);
     code = strcat(code,presetNew);
     presetNew = [];
     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Letter_',num2str(10+i),'.png']);
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Letter_',num2str(10+i),'.png']);
 end
-% 
-% imagefiles = dir(fullfile(cwd, ('arial*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj12_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj12_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj12_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('Comicsans*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj13_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj13_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj13_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('Curlz*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj14_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj14_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj14_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('elephant*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj15_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj15_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj15_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('impact*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj16_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj16_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj16_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('Papyrus*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj17_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj17_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj17_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('roman*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj18_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj18_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj18_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('snap*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj19_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj19_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj19_',num2str(10+i),'.png']);
-% end
-% 
-% imagefiles = dir(fullfile(cwd, ('Terminal*.png')));
-% nfiles = length(imagefiles);    % Number of files found
-% for i = 1:nfiles
-%     presetNew = strrep(preset,'Object',['Obj20_',num2str(10+i)]);
-%     presetNew = strrep(presetNew,'IDN',['Obj20_',num2str(10+i)]);
-%     code = strcat(code,presetNew);
-%     presetNew = [];
-%     % rename image file
-%     copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj20_',num2str(10+i),'.png']);
-% end
+
+imagefiles = dir(fullfile(cwd, ('arial*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj12_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj12_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj12_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('Comicsans*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj13_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj13_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj13_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('Curlz*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj14_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj14_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj14_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('elephant*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj15_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj15_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj15_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('impact*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj16_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj16_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj16_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('Papyrus*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj17_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj17_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj17_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('roman*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj18_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj18_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj18_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('snap*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj19_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj19_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj19_',num2str(10+i),'.png']);
+end
+
+imagefiles = dir(fullfile(cwd, ('Terminal*.png')));
+nfiles = length(imagefiles);    % Number of files found
+for i = 1:nfiles
+    presetNew = strrep(preset,'Object',['Obj20_',num2str(10+i)]);
+    presetNew = strrep(presetNew,'IDN',['Obj20_',num2str(10+i)]);
+    code = strcat(code,presetNew);
+    presetNew = [];
+    % rename image file
+    copyfile(fullfile(cwd,[imagefiles(i).name]),['Obj20_',num2str(10+i),'.png']);
+end
 
 % code2 = [];
 % imgnum = ['x,'];
