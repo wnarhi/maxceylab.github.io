4d28306bbed91810c9de5118f7c9fea7068323b9
PaulScotti <scottibrain@gmail.com>
2019-01-18 10:44:57 -0500

    lettersz

--- expts/letters/initVariables.js
+++ expts/letters/initVariables.js
@@ -2,14 +2,14 @@ let CDSet = chance1.shuffle(repmat(perm_concat([0,1],counter(1,25)),4)); // rand
 
 let Fonts = chance1.shuffle(counter(11,20)); // 10 fonts/categories
 
-let RpCats = Fonts.slice(0,5);
-let NrpCats = Fonts.slice(5,10);
-let RpExemSlice = chance1.shuffle(counter(11, 34)); // letters I and O were excluded from imagesets due to lacking characteristic features
+let RpCats = Fonts.slice(0,5); // 5 categories
+let NrpCats = Fonts.slice(5,10); // 5 categories
+let RpExemSlice = chance1.shuffle(counter(11, 32)); // letters I and O were excluded from imagesets due to lacking characteristic features; A and Z excluded due to instructions
 let RpPlusExem = RpExemSlice.slice(0, 3); //type 0
 let RpMinusExem = RpExemSlice.slice(3, 6); //type 1
 let PracLuresExem = RpExemSlice.slice(6, 12); //type 3
 let RpTestLuresExem = RpExemSlice.slice(12, 18); //type 4
-let NrpExemSlice = chance1.shuffle(counter(11, 34));
+let NrpExemSlice = chance1.shuffle(counter(11, 32));
 let NrpExem = NrpExemSlice.slice(0, 6); //type 2
 let NrpTestLuresExem = NrpExemSlice.slice(6, 12); //type 5
 
