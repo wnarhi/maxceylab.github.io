a9e03aa3e812f3afd1acd57217aa60e054f34df9
PaulScotti <scottibrain@gmail.com>
2019-02-18 15:59:11 -0500

    asdf

--- expts/compelling_scenes/initVariables.js
+++ expts/compelling_scenes/initVariables.js
@@ -42,7 +42,7 @@ let AllStudy = []
 AllStudy = AllStudy.concat(RpPlus,RpMinus,Nrp);
 AllStudy = AllStudy.sort();
 let AllImages = [];
-AllImages = AllImages.concat(RpPlus,RpMinus,Nrp,PracLures,RpTestLures,NrpTestLures,FourthTestLuresExem);
+AllImages = AllImages.concat(RpPlus,RpMinus,Nrp,PracLures,RpTestLures,NrpTestLures);
 
 let RpPlus_StudyOrder = [];
 let RpMinus_StudyOrder = [];
