3783517dff77fcd2505df7cf3933b1c8a88058b6
PaulScotti <scottibrain@gmail.com>
2019-02-21 14:00:12 -0500

    ring

--- expts/retrieval/initVariables.js
+++ expts/retrieval/initVariables.js
@@ -1,15 +1,15 @@
 let CDSet = chance1.shuffle(repmat(perm_concat([0,1],counter(1,25)),4)); // randomized 200 CD trials
 
-let Cats = chance1.shuffle([01,04,07,08,10,11,12,13,14,15,17,18,02,09,16]);
+let Cats = chance1.shuffle([01,02,03,06,07,11,14,15]); // 8 cats
+// has 26: 02 04 05 08 10
 
 // ensure that RPCats does not contain any category that has less than 18 exemplars
-while (Cats.slice(0,5).includes(02) || Cats.slice(0,5).includes(09) || Cats.slice(0,5).includes(16)) {
-  Cats = chance1.shuffle([01,04,07,08,10,11,12,13,14,15,17,18,02,09,16]);
+while (Cats.slice(0,5).includes(02)) {
+  Cats = chance1.shuffle([01,02,03,06,07,11,14,15]);
 }
 
-let RpCats = Cats.slice(0,5); // 5 categories
-let NrpCats = Cats.slice(5,10); // 5 categories
-let ForgetCats = Cats.slice(10,15); // 5 categories
+let RpCats = Cats.slice(0,4); // 4 categories
+let NrpCats = Cats.slice(4,8); // 4 categories
 
 let RpExemSlice = chance1.shuffle(counter(11, 28));
 let RpPlusExem = RpExemSlice.slice(0, 3); //type 0
@@ -19,9 +19,6 @@ let RpTestLuresExem = RpExemSlice.slice(12, 18); //type 4
 let NrpExemSlice = chance1.shuffle(counter(11, 22));
 let NrpExem = NrpExemSlice.slice(0, 6); //type 2
 let NrpTestLuresExem = NrpExemSlice.slice(6, 12); //type 5
-let ForgetExemSlice = chance1.shuffle(counter(11,22))
-let ForgetExem = NrpExemSlice.slice(0, 6); //type 6
-let ForgetLuresExem = NrpExemSlice.slice(6, 12); //type 7
 
 let StudyTrialType = [];
 StudyTrialType = StudyTrialType.concat(repmat(repmat([0, 1], RpPlusExem.length), RpCats.length), repmat(repmat(2, NrpExem.length), NrpCats.length), repmat(repmat(6, ForgetExem.length), ForgetCats.length));
@@ -44,15 +41,12 @@ let Nrp = chance1.shuffle(perm_concat(NrpCats,NrpExem));
 let PracLures = chance1.shuffle(perm_concat(RpCats,PracLuresExem));
 let RpTestLures = chance1.shuffle(perm_concat(RpCats,RpTestLuresExem));
 let NrpTestLures = chance1.shuffle(perm_concat(NrpCats,NrpTestLuresExem));
-let Forget = chance1.shuffle(perm_concat(ForgetCats,ForgetExem));
-let ForgetLures = chance1.shuffle(perm_concat(ForgetCats,ForgetLuresExem));
 let AllImages = [];
-AllImages = AllImages.concat(RpPlus,RpMinus,Nrp,PracLures,RpTestLures,NrpTestLures,Forget,ForgetLures);
+AllImages = AllImages.concat(RpPlus,RpMinus,Nrp,PracLures,RpTestLures,NrpTestLures);
 
 let RpPlus_StudyOrder = [];
 let RpMinus_StudyOrder = [];
 let Nrp_StudyOrder = [];
-let Forget_StudyOrder = [];
 
 function repmat(array, count) {
   let result = [];
