bdafc28d792675788d0739aba62643fd329e4e15
PaulScotti <scottibrain@gmail.com>
2019-03-27 09:03:26 -0400

    rp colors

--- expts/retrieval/initVariables.js
+++ expts/retrieval/initVariables.js
@@ -45,11 +45,11 @@ TestTrialType = chance1.shuffle(TestTrialType);
 
 // let RandStart = Math.floor(chance1.random()*360)+1; // random number from 1 to 360
 let RpPlus = chance1.shuffle(perm_concat(RpCats,RpPlusExem));
-let RpPlusC = wrap(RandStart(counter(1,360,360/RpPlus.length)));
+let RpPlusC = wrap(RandStart(counter(1,180,180/RpPlus.length)));
 let RpPlusPrac = [];
 RpPlusPrac = RpPlusPrac.concat(chance1.shuffle(perm_concat(RpCats,RpPlusExem)),chance1.shuffle(perm_concat(RpCats,RpPlusExem))); //twice because of two practice trials
 let RpMinus = chance1.shuffle(perm_concat(RpCats,RpMinusExem));
-let RpMinusC = wrap(RandStart(counter(1,360,360/RpMinus.length)));
+let RpMinusC = wrap(RandStart(counter(1,180,180/RpMinus.length)));
 let Nrp = chance1.shuffle(perm_concat(NrpCats,NrpExem));
 let NrpC = wrap(RandStart(counter(1,360,360/Nrp.length)));
 let PracLures = chance1.shuffle(perm_concat(RpCats,PracLuresExem));
