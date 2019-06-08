ac06e874527ec7108082e71577436d8201e9c7b6
PaulScotti <scottibrain@gmail.com>
2019-06-07 21:07:12 -0400

    awef

--- expts/retrieval/initVariables.js
+++ expts/retrieval/initVariables.js
@@ -78,7 +78,7 @@ let RpTestLuresRC = wrap(RandStart(counter(1,360,Math.round(360/RpTestLuresR.len
 let NrpTestLuresR = chance1.shuffle(perm_concat(NrpCatsR,NrpTestLuresExemR));
 let NrpTestLuresRC = wrap(RandStart(counter(1,360,Math.round(360/NrpTestLuresR.length))));
 
-let Repeats = chance1.shuffle(counter(1,StudyTrialType.length)).slice(0,24); // 24/(288+24) = 7.69% of trials are repeats
+let Repeats = chance1.shuffle(counter(2,StudyTrialType.length-1)).slice(0,24); // 24/(288+24) = 7.69% of trials are repeats
 
 let AllImages = [];
 AllImages = AllImages.concat(RpPlus,RpMinus,Nrp,PracLures,RpTestLures,NrpTestLures,
